a3d6a3ea5abb1db68a959fa40d0709be39c0e11f
Chris Yang <chris.m.yang@gmail.com>
2012-06-28 16:39:42 -0400

    Expressions that don't define values (i.e. class, imports, etc.) do sensible things in notebook.  Adding TODO file also.

--- client/src/main/scala/NotebookKernel.scala
+++ client/src/main/scala/NotebookKernel.scala
@@ -28,6 +28,8 @@ class NotebookKernel extends Actor {
   lazy val interp = {
     val settings = new Settings
     settings.embeddedDefaults[NotebookKernel]
+
+    // CY: this is necessary, at least for me, for the repl to work when run from IntelliJ
     settings.usejavacp.value = true
     val i = new HackIMain(settings, stdout)
     i.initializeSynchronous()
@@ -37,28 +39,31 @@ class NotebookKernel extends Actor {
     case ExecuteRequest(code) =>
       stdout.flush()
       stdoutBytes.reset()
-      val res = interp.interpret(code, true)
+      // capture stdout if the code the user wrote was a println, for example
+      val res = Console.withOut(stdoutBytes) {
+        interp.interpret(code)
+      }
       stdout.flush()
       if (res == Success) {
         val request = interp.previousRequests.last
-        val line = request.lineRep
-        // CY: So for whatever reason, line.evalValue attemps to call the $eval method
-        // on the class...a method that does not exist.  Not sure if this is a bug in the
-        // REPL or some artifact of how we are calling it.
-        val foo = line.call("$result")
-        println("tree: " + request.trees.mkString("\n"))
-        println("eval value: " + request.lineRep.evalValue)
-        println("eval class: " + request.lineRep.evalClass.getMethods.mkString("\n"))
-//        println("evalCaught: " + request.lineRep.evalCaught.foreach(_.printStackTrace()))
-        request.lineRep.evalCaught.foreach(_.printStackTrace())
-        println("class: " + request.getEvalTyped.toString)
-        println("Reuqest:" + request)
-        println("Value of term: " + interp.valueOfTerm("res0"))
-        val value = request.getEval.getOrElse("empty").toString
-        println("Most recent var: " + value)
-//        sender ! ExecuteResponse(stdoutBytes.toString)
+        val handlers = request.handlers
+        val last = handlers.last
+        val evalValue = if (handlers.last.definesValue) {
+          // CY: So for whatever reason, line.evalValue attemps to call the $eval method
+          // on the class...a method that does not exist.  Not sure if this is a bug in the
+          // REPL or some artifact of how we are calling it.
+          val line = request.lineRep
+          // a line like println(...) is technically a val, but returns null for some reason
+          // so wrap it in an option in case that happens...
+          val result = Option(line.call("$result"))
+          result.map(_.toString)
+        } else {
+          None
+        }
 
-        sender ! ExecuteResponse(foo.toString)
+        val valueToSend = evalValue.getOrElse(stdoutBytes.toString)
+
+        sender ! ExecuteResponse(valueToSend)
       } else
         sender ! ErrorResponse(stdoutBytes.toString)
   }
